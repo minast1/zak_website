2728908894777a1178ec8bad8a4ef7a89ac1687a
minast1 <edmarfo2@gmail.com>
2025-04-23 01:09:58 +0000

    add return button to edit page

--- src/app/zachary-online/v1/dashboard/[slug]/page.tsx
+++ src/app/zachary-online/v1/dashboard/[slug]/page.tsx
@@ -1,8 +1,10 @@
 //import { getCurrentUser } from "@/lib/firebase/firebase-admin";
 import { fetchPostBySlug } from "@/app/actions/posts/post";
 import EditPostForm from "@/components/dashboard/edit-post-form";
+import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { EditIcon } from "lucide-react";
+import Link from "next/link";
 
 export default async function PostEditPage({
   params,
@@ -15,11 +17,14 @@ export default async function PostEditPage({
   return (
     <main className="space-y-10">
       <div>
-        <Card className="shadow-xl">
-          <CardHeader className="px-6 border-b border-gray-200 py-4">
-            <CardTitle className="font-bold text-xl text-gray-800 flex items-center gap-2">
+        <Card>
+          <CardHeader className="px-6 border-b border-gray-200 py-4 flex flex-row justify-between">
+            <CardTitle className="font-bold text-xl text-gray-800 flex items-center gap-2 flex-1">
               Edit Post <EditIcon className="h-4 w-4" />
             </CardTitle>
+            <Button size={"sm"} asChild className="max-w-fit">
+              <Link href="/zachary-online/v1/dashboard">Back to Posts</Link>
+            </Button>
           </CardHeader>
           <CardContent>
             <EditPostForm
