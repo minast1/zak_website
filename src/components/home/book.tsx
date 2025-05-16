79550154e1bd2536e0286836d41ea176a097ce9c
minast1 <edmarfo2@gmail.com>
2025-05-16 02:27:16 +0000

    change production url

--- src/components/home/book.tsx
+++ src/components/home/book.tsx
@@ -11,7 +11,7 @@ import { useMediaQuery } from "@/hooks/useMediaQuery";
 
 pdfjs.GlobalWorkerOptions.workerSrc =
   process.env.NODE_ENV === "production"
-    ? `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.mjs`
+    ? `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`
     : new URL(
         "pdfjs-dist/build/pdf.worker.min.mjs",
         import.meta.url
